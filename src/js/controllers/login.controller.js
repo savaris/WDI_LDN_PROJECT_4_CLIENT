7d3f09644237c4596b196410f6c519d840da27e2
savaris <hudhayfajamalkhan@gmail.com>
2017-02-20 11:29:50 +0000

    Created Angular Users Controller

---
+++ src/js/controllers/login.controller.js
@@ -0,0 +1,19 @@
+angular
+  .module('gameLibrary')
+  .controller('LoginCtrl', LoginCtrl);
+
+LoginCtrl.$inject = ['User', 'CurrentUserService'];
+function LoginCtrl(User, CurrentUserService) {
+  const vm = this;
+
+  vm.login = () => {
+    User
+    .login(vm.user)
+    .$promise
+    .then(() => {
+      CurrentUserService.getUser();
+    }, err => {
+      console.log(err);
+    });
+  };
+}
---
+++ src/js/controllers/register.controller.js
@@ -0,0 +1,18 @@
+angular
+  .module('gameLibrary')
+  .controller('RegisterCtrl', RegisterCtrl);
+
+RegisterCtrl.$inject = ['User'];
+function RegisterCtrl(User){
+  const vm = this;
+
+  vm.register = () => {
+    User.register(vm.user)
+    .$promise
+    .then(data => {
+      console.log(data);
+    }, err => {
+      console.log(err);
+    });
+  };
+}
