f381f4b369d2383cfff18db170dfae62e9904fa8
savaris <hudhayfajamalkhan@gmail.com>
2017-03-11 18:31:06 +0000

    Heroku HTTPS Constnat

--- src/js/constants/api.constant.js
+++ src/js/constants/api.constant.js
@@ -1,4 +1,4 @@
 angular
   .module('gameLibrary')
   // .constant('API', `http://localhost:3000`);
-  .constant('API', 'http://wdi-project-4-api-hudhayfa.herokuapp.com');
+  .constant('API', 'https://wdi-project-4-api-hudhayfa.herokuapp.com');
